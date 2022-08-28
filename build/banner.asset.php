772f0fea1670c08076138b703401bef3e9d4d89a
boonruang <scubatoy@gmail.com>
2022-08-29 03:19:13 +0700

    167. Making Our Block Editable

--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '1a827575fdb55a5775cf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => 'e8054c4220c8e7aa155c');
