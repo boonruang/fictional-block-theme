a802b0d92d6f6211861878088440843a1d16affd
boonruang <scubatoy@gmail.com>
2022-08-29 01:24:32 +0700

    166. Custom Banner Block (Part 2)

---
+++ build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '1a827575fdb55a5775cf');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fc4f024a0fb38bff156c');
