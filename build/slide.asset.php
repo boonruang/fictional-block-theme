8db15e4b6e1a255e72baadbf9aabff9581ff198d
boonruang <scubatoy@gmail.com>
2022-08-30 11:32:28 +0700

    185. Slideshow Block (Part 1)

---
+++ build/slide.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f8781e9b4e5317a897db');
---
+++ build/slideshow.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '478c146dd784f83b8c69');
